de3fb4ce8eee2f1ab691d484725ba4331567ad25
zmad5306 <zachary.maddox@gmail.com>
2017-09-30 12:43:28 -0400

    added simple tc example

---
+++ thermocouple/thermocouple.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+#define FAN     11
+
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+
+void setup() {
+  Serial.begin(9600);
+ 
+  while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
+
+  // Serial.println("MAX31855 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+}
+
+void loop() {
+   Serial.print("Internal Temp = ");
+   Serial.println(thermocouple.readInternal());
+
+   double c = thermocouple.readCelsius();
+   double f = thermocouple.readFarenheit();
+   
+   if (isnan(c) || isnan(f)) {
+     Serial.println("Something wrong with thermocouple!");
+   } else {
+     Serial.print("C = "); 
+     Serial.println(c);
+     Serial.print("F = ");
+     Serial.println(f);
+   }
+
+   delay(100);
+}
